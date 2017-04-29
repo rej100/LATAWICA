a3a3ee1f5d7bdaac6921d95139b1b652736c3467
uslessbot <rowerek0205@gmail.com>
2017-04-29 15:04:53 +0200

    ewfweojf
    ewfweopfjwefw update v`1.8

--- LatawicaBhop/LatawicaBhop/BhopT.h
+++ LatawicaBhop/LatawicaBhop/BhopT.h
@@ -13,19 +13,54 @@ using std::endl;
 namespace BhopT
 {
 	bool jumped = false;
+
+	bool toggle = true;
+
+	void ToggleL()
+	{
+		while (true)
+		{
+			if (GetAsyncKeyState(VK_F1) & 0x1)
+			{
+				if (toggle)
+				{
+					toggle = false;
+					system("cls");
+					cout << "Waiting for CS:GO..." << endl;
+					cout << "Configuring..." << endl;
+					cout << "Ready for Bhopping :)" << endl;
+					cout << "Toggle bhop F1 ";
+					cout << "OFF";
+					}
+				else if (!toggle)
+				{
+					toggle = true;
+					system("cls");
+					cout << "Waiting for CS:GO..." << endl;
+					cout << "Configuring..." << endl;
+					cout << "Ready for Bhopping :)" << endl;
+					cout << "Toggle bhop F1 ";
+					cout << "ON";
+				}
+			}
+		}
+	}
+
 	void DoBhop(MemoryManagment *mem)
 	{
 		while (true) 
 		{
-				if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && !Utils::PlayerInAir(mem)) 
+			if (toggle)
+			{
+				if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && !Utils::PlayerInAir(mem))
 				{
-					SendMessage(mem -> processHWND, WM_KEYDOWN, VK_SPACE, 0x390000);
+					SendMessage(mem->processHWND, WM_KEYDOWN, VK_SPACE, 0x390000);
 					jumped = true;
 				}
-				else if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && Utils::PlayerInAir(mem)) 
+				else if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && Utils::PlayerInAir(mem))
 				{
-					SendMessage(mem -> processHWND, WM_KEYUP, VK_SPACE, 0x390000);
-					if (jumped) 
+					SendMessage(mem->processHWND, WM_KEYUP, VK_SPACE, 0x390000);
+					if (jumped)
 					{
 						Sleep(16);
 						SendMessage(mem->processHWND, WM_KEYDOWN, VK_SPACE, 0x390000);
@@ -33,10 +68,12 @@ namespace BhopT
 						jumped = false;
 					}
 				}
-				else 
+				else
 				{
 					Sleep(1);
 				}
+			}
+			Sleep(1);
 		}
 	}
 
@@ -45,9 +82,10 @@ namespace BhopT
 		cout << "Configuring..." << endl;
 		Sleep(2000);
 		cout << "Ready for Bhopping :)" << endl;
-
+		cout << "Toggle bhop F1 ";
+		thread ToggleR(ToggleL);
 		thread BhopThread(DoBhop, mem);
-		BhopThread.join();
-
+		ToggleR.join();
+		BhopThread.join();		
 	}
 }
